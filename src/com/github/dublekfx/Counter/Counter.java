714e6e42cab2bc44c61d2e3e0dab7340f641527f
Keiko Kaplan <dublekfx@gmail.com>
2013-06-02 22:03:20 -0400

    Trying to figure out how to cancel the task

--- src/com/github/dublekfx/Counter/Counter.java
+++ src/com/github/dublekfx/Counter/Counter.java
@@ -1,5 +1,39 @@
 package com.github.dublekfx.Counter;
 
-public class Counter {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+public class Counter extends JavaPlugin {
+	
+	private BukkitTask task;
+	private int playerLevel;
+	
+	@Override
+	public void onEnable()	{
+		
+	}
+	@Override
+	public void onDisable()	{
+		
+	}
+	
+	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args)	{
+		
+		
+		return false;		
+	}
+	
+	public void startCounter(Player p, int tStart)	{
+		playerLevel = p.getLevel();
+		p.setLevel(tStart);
+		task = new CounterClock(this, p, playerLevel).runTaskTimer(this, 20, 20);
+		
+	}
+	public void endCounter(Player p, int l)	{
+		
+	}
 
 }
--- src/com/github/dublekfx/Counter/CounterClock.java
+++ src/com/github/dublekfx/Counter/CounterClock.java
@@ -1,5 +1,34 @@
 package com.github.dublekfx.Counter;
 
-public class CounterClock {
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class CounterClock extends BukkitRunnable	{
+
+	private JavaPlugin plugin;
+	private Player p;
+	public BukkitTask task;
+	private int pLevel;
+	
+	public CounterClock(JavaPlugin pl, Player play, int level)	{
+		plugin = pl;
+		p = play;
+		pLevel = level;
+	}
+	
+	@Override
+	public void run() {
+		if (p.getLevel() >= 1) {
+			p.setLevel(p.getLevel() - 1);
+		}
+		else	{
+			p.setLevel(pLevel);
+			task.cancel();
+		}
+			
+		
+	}
 
 }
